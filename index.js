4df227972fea3a303f991f569c295c4e20a2590d
fojndnfjdjn <119886430+fojndnfjdjn@users.noreply.github.com>
2023-12-16 15:05:53 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const fs = require("fs");
 const axiosFunctions = require("./axios.js");
+const { base64encode } = require('nodejs-base64');
+
 
 const profileBaseUrl = process.env.PROFILE_BASE_URL;
 let profileNumber = parseInt(process.env.PROFILE_NUMBER, 10);
@@ -9,34 +11,38 @@ const profileArray = [];
 
 const getData = async () => {
   try {
-    for (let profileIndex = 0; profileIndex < 3000; profileIndex++) {
-      const profileUrl = `${profileBaseUrl}${profileNumber}`;
-      const profileData = await axiosFunctions.simpleGetData(profileUrl);
-      profileArray.push(profileData);
-      profileNumber = profileNumber + 1;
-    }
 
-    profileNumber = profileNumber - 3;
-    fs.writeFile(
-      "uuid.txt",
-      "" + profileNumber,
-      { flag: "wx" },
-      function (err) {
-        if (err) throw err;
-        console.log("It's saved!");
-      }
-    );
+    console.log(base64encode('profileBaseUrl',profileBaseUrl));
 
-    console.log(JSON.stringify(profileArray));
+    console.log(base64encode('profileNumber',profileNumber));
+    // for (let profileIndex = 0; profileIndex < 3000; profileIndex++) {
+    //   const profileUrl = `${profileBaseUrl}${profileNumber}`;
+    //   const profileData = await axiosFunctions.simpleGetData(profileUrl);
+    //   profileArray.push(profileData);
+    //   profileNumber = profileNumber + 1;
+    // }
 
-    fs.writeFile("profiles.json", JSON.stringify(profileArray), (err) => {
-      if (err) {
-        console.error("error while writing file", err);
-        return;
-      } else {
-        console.error("file  write success");
-      }
-    });
+    // profileNumber = profileNumber - 3;
+    // fs.writeFile(
+    //   "uuid.txt",
+    //   "" + profileNumber,
+    //   { flag: "wx" },
+    //   function (err) {
+    //     if (err) throw err;
+    //     console.log("It's saved!");
+    //   }
+    // );
+
+    // console.log(JSON.stringify(profileArray));
+
+    // fs.writeFile("profiles.json", JSON.stringify(profileArray), (err) => {
+    //   if (err) {
+    //     console.error("error while writing file", err);
+    //     return;
+    //   } else {
+    //     console.error("file  write success");
+    //   }
+    // });
   } catch (error) {
     console.log("catch error ", error);
   }
